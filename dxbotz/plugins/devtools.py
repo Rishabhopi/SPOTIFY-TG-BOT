b0c60dd997952c1f34b0ab2cbc27d02beb175fb9
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-01-22 23:33:55 +0530

    Update devtools.py

--- dxbotz/plugins/devtools.py
+++ dxbotz/plugins/devtools.py
@@ -2,6 +2,7 @@
 #Licensed under the  AGPL-3.0 License;
 #you may not use this file except in compliance with the License.
 #Author ZIYAN
+#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
 
 import sys
 import requests
