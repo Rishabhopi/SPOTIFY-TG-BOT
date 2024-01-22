2f4d3986725701afef5b8c7fc01a6efdc51a46f7
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-01-22 23:33:44 +0530

    Update deezer.py

--- dxbotz/plugins/deezer.py
+++ dxbotz/plugins/deezer.py
@@ -2,6 +2,8 @@
 #Licensed under the  AGPL-3.0 License;
 #you may not use this file except in compliance with the License.
 #Author ZIYAN
+#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
+
 from pyrogram import filters
 from config import AUTH_CHATS, LOG_GROUP
 from dxbotz import Dxbotz
