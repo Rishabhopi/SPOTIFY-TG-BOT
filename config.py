d82723059fa6afe56f5629f6002a02d51db5a7d0
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-01-22 23:37:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2024 DX-MODS
-#Licensed under the MIT License;
+#Licensed under the  AGPL-3.0 License;
 #you may not use this file except in compliance with the License.
 #Author ZIYAN
+#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
 
 import re
 import os
