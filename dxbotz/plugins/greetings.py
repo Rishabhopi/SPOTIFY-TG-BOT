9bdf7d755ec219b62d51dcd988c3638683651b0b
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-01-22 23:34:49 +0530

    Update greetings.py

--- dxbotz/plugins/greetings.py
+++ dxbotz/plugins/greetings.py
@@ -2,6 +2,8 @@
 #Licensed under the  AGPL-3.0 License;
 #you may not use this file except in compliance with the License.
 #Author ZIYAN
+#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
+
 from datetime import datetime
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup, Message
