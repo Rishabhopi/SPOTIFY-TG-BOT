9ae95beb71c38dc11d9facbe0f6557fa653e043d
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2024-01-22 23:28:21 +0530

    year updated

--- dxbotz/plugins/facebook.py
+++ dxbotz/plugins/facebook.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2024 DX-MODS
+#Licensed under the MIT License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
+
 from pyrogram import filters, Client
 import bs4, requests,re,asyncio
 import wget,os,traceback
